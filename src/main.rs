611b3e948bdbfe3c545cf37ad81eba0090a37b3f
Michael Berry <trismegustis@gmail.com>
2019-12-14 18:27:39 -0500

    Changed help messages.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args.len() {
-        2 => help(&args[0]),
+        2 => {
+            if &args[1] == "help" {
+                help(&args[0]);
+            } else if &args[1] == "ftoc" {
+                eprintln!("Add a degree Fahrenheit to convert to Celsius.");
+            } else if &args[1] == "ctof" {
+                eprintln!("Add a degree Celsius to convert to Fahrenheit.");
+            } else {
+                eprintln!("Error: Unknown command!\n");
+                help(&args[0]);
+            }
+        }
         3 => run_command(args),
         _ => run_interactive_loop(),
     }
@@ -29,15 +40,19 @@ fn run_command(args: Vec<String>) {
             Ok(t) => print_temperature(&Temperature::C(t)),
             Err(_) => help(prog),
         },
-        _ => println!("Invalid command!"),
+        _ => eprintln!("Invalid command!"),
     }
 }
 
 /// Display help
 fn help(prog: &str) {
-    println!("{} ftoc degrees_fahrenheit", prog);
-    println!("{} ctof degrees_celsius", prog);
-    println!("{} interactive program", prog);
+    eprintln!("Valid commands\n");
+    eprintln!("{} ftoc degrees_fahrenheit", prog);
+    eprintln!("\tConvert given degrees Fahrenheit to Celsius\n");
+    eprintln!("{} ctof degrees_celsius", prog);
+    eprintln!("\tConvert given degrees Celsius to Fahrenheit\n");
+    eprintln!("{}", prog);
+    eprintln!("\tRun interactive program");
 }
 
 /// Run an interactive loop of the program
@@ -64,7 +79,7 @@ fn run_interactive_loop() {
         let choice: u8 = match choice.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("\nInvalid choice!\n");
+                eprintln!("\nInvalid choice!\n");
                 continue;
             }
         };
@@ -75,7 +90,7 @@ fn run_interactive_loop() {
                 match degrees {
                     Ok(t) => print_temperature(&Temperature::F(t)),
                     Err(e) => {
-                        println!("{}", e);
+                        eprintln!("{}", e);
                         continue;
                     }
                 }
@@ -85,12 +100,12 @@ fn run_interactive_loop() {
                 match degrees {
                     Ok(t) => print_temperature(&Temperature::C(t)),
                     Err(e) => {
-                        println!("{}", e);
+                        eprintln!("{}", e);
                         continue;
                     }
                 }
             }
-            _ => println!("\nEnter 1, 2, exit, or quit!\n"),
+            _ => eprintln!("\nEnter 1, 2, exit, or quit!\n"),
         }
     }
 }
